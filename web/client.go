bf7c98258724178e975d7a44e4c07e020e2190aa
Sheva Xu <shevaxu@outlook.com>
2018-01-12 19:23:12 +0800

    feat (web): add client side utils

---
+++ web/client.go
@@ -0,0 +1,194 @@
+// Package web provides useful features to Go's http.Client
+// following some best practices for production,
+// such as timeout, retries and backoff.
+package web
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"math/rand"
+	"net"
+	"net/http"
+	"time"
+)
+
+// NewJSONPost returns a Request with json encoded and header set.
+func NewJSONPost(url string, v interface{}) (*http.Request, error) {
+	data, err := json.Marshal(v)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Content-Type", "application/json; charset=utf-8")
+	return req, nil
+}
+
+// RequestWithClose sends the request and returns statusCode and raw body.
+// It reads and closes Response.Body, return any error occurs.
+func RequestWithClose(cl *http.Client, req *http.Request) (status int, body []byte, err error) {
+	var resp *http.Response
+
+	resp, err = cl.Do(req)
+	if err != nil {
+		return
+	}
+
+	// Close() iff resp did return
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+
+	status = resp.StatusCode
+
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+// ShouldRetry determines if the client should repeat the request
+// without modifications at any later time;
+// returns true for http 408 and 5xx status.
+func ShouldRetry(statusCode int) bool {
+	// TODO: should exclude 501, 505 and 511?
+	return statusCode == http.StatusRequestTimeout ||
+		(statusCode >= 500 && statusCode <= 599)
+}
+
+// IsTimeoutErr checks if the error is a timeout.
+func IsTimeoutErr(e error) bool {
+	if err, ok := e.(net.Error); ok {
+		return err.Timeout()
+	}
+	return false
+}
+
+// Backoff implements the exponential backoff algorithm
+// with jitter for performing remote calls.
+// It use an alternative method as described in
+// https://www.awsarchitectureblog.com/2015/03/backoff.html.
+type Backoff struct {
+	BaseSleep, MaxSleep int
+}
+
+// Next returns the next sleep time computed by the previous one;
+// the *Decorrelated Jitter* is:
+// sleep = min(cap, random_between(base, sleep * 3)).
+func (b *Backoff) Next(previous int) int {
+	diff := previous*3 - b.BaseSleep
+	// Intn will panic if arg <= 0
+	if diff <= 0 {
+		diff = 1
+	}
+	sleep := rand.Intn(diff) + b.BaseSleep
+	if sleep > b.MaxSleep {
+		return b.MaxSleep
+	}
+	return sleep
+}
+
+// Client provides additional features upon http.Client,
+// e.g., io Reader handle and request retry with backoff.
+type Client interface {
+	// Do sends the request with at most maxTries time.
+	// Retries happen in following conditions:
+	// 1. timeout error occurs;
+	// 2. should-retry status code is returned.
+	// It also normalize the HTTP response as:
+	// (tries, status int, body []byte, err error),
+	// for #requests made, status code for the final request,
+	// response body and error respectively.
+	Do(req *http.Request, maxTries int) (tries, status int, body []byte, err error)
+}
+
+// client implements the Client interface.
+// It wraps a http.Client underneath
+// (safe for concurrent use by multiple goroutines).
+type client struct {
+	timeoutOnly bool // only retry for timeout error
+	cl          *http.Client
+	bk          Backoff
+}
+
+// NOTICE: retry works for request with no body only before go1.9.
+func (c *client) Do(req *http.Request, maxTries int) (tries, status int, body []byte, err error) {
+	// 0 will trigger setting wait to base
+	wait := 0
+
+	for tries = 1; tries <= maxTries; tries++ {
+		// update next sleep time
+		wait = c.bk.Next(wait)
+		// do request
+		status, body, err = RequestWithClose(c.cl, req)
+		if err != nil {
+			if !c.timeoutOnly || IsTimeoutErr(err) {
+				time.Sleep(time.Duration(wait) * time.Millisecond)
+				continue
+			}
+			return
+		}
+		// no error, check status
+		if ShouldRetry(status) {
+			time.Sleep(time.Duration(wait) * time.Millisecond)
+			continue
+		}
+		// succeed or should not repeat
+		return
+	}
+
+	// return the last request's response, succeed or not
+	tries--
+	return
+}
+
+// ClientOption allows functional pattern options for client.
+type ClientOption func(*client)
+
+// TimeoutOnly sets the client to retry only
+// on timeout error instead of all errors.
+func TimeoutOnly() ClientOption {
+	return func(c *client) {
+		c.timeoutOnly = true
+	}
+}
+
+// WithHTTPClient substitutes the default 5s
+// timeout http.Client with a custom one.
+func WithHTTPClient(cl *http.Client) ClientOption {
+	return func(c *client) {
+		c.cl = cl
+	}
+}
+
+// WithBackoff substitutes the default Backoff.
+func WithBackoff(b Backoff) ClientOption {
+	return func(c *client) {
+		c.bk = b
+	}
+}
+
+// NewClient returns a client with default setting:
+// 1. retry on all errors;
+// 2. http.Client set Timeout to 5s;
+// 3. Backoff{100, 5000}.
+func NewClient(ops ...ClientOption) Client {
+	c := &client{
+		timeoutOnly: false, // retry all errors
+		cl:          &http.Client{Timeout: 5 * time.Second},
+		bk:          Backoff{100, 5000},
+	}
+
+	for _, op := range ops {
+		op(c)
+	}
+
+	return c
+}
---
+++ web/client_test.go
@@ -0,0 +1,285 @@
+package web_test
+
+import (
+	"encoding/json"
+	"errors"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/ShevaXu/golang/assert"
+	"github.com/ShevaXu/golang/web"
+)
+
+func TestNewJSONPost(t *testing.T) {
+	type testContent struct {
+		Data string `json:"data"`
+	}
+
+	a := assert.NewAssert(t)
+
+	req, err := web.NewJSONPost("/", testContent{"hello"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	a.Equal("application/json; charset=utf-8", req.Header.Get("Content-Type"), "Proper header")
+
+	decoder := json.NewDecoder(req.Body)
+	var c testContent
+	err = decoder.Decode(&c)
+	a.NoError(err, "Error decoding the body: %s")
+	a.Equal(c.Data, "hello", `Should respond with "hello"`)
+}
+
+func DummyHandler(code int, content []byte) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(code)
+		w.Write(content)
+	}
+}
+
+func SleepHandler(d time.Duration) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		time.Sleep(d)
+	}
+}
+
+var (
+	okResp    = []byte("OK")
+	errResp   = []byte("exception")
+	okHandler = DummyHandler(http.StatusOK, okResp)
+)
+
+type closeTest struct {
+	h             http.Handler
+	expectedCode  int
+	expectedBody  []byte
+	expectTimeout bool
+}
+
+// TODO: how to test if Close() works
+func TestRequestWithClose(t *testing.T) {
+	a := assert.NewAssert(t)
+
+	tests := []closeTest{
+		{
+			okHandler,
+			http.StatusOK,
+			okResp,
+			false,
+		},
+		{
+			SleepHandler(20 * time.Millisecond),
+			http.StatusOK,
+			nil,
+			true,
+		},
+	}
+
+	cl := &http.Client{Timeout: 10 * time.Millisecond}
+
+	for _, test := range tests {
+		server := httptest.NewServer(test.h)
+
+		req, err := http.NewRequest("GET", server.URL, nil)
+		if err != nil {
+			t.Errorf("Error new request: %s", err)
+			continue
+		}
+		status, body, err := web.RequestWithClose(cl, req)
+		if test.expectTimeout {
+			a.NotNil(err, "Should return timeout error")
+			a.Equal(true, web.IsTimeoutErr(err), "Should be a timeout")
+		} else {
+			a.NoError(err, "Request succeeds")
+			a.Equal(test.expectedCode, status, "Returns code")
+			a.Equal(test.expectedBody, body, "Returns body")
+		}
+
+		server.Close()
+	}
+}
+
+func TestShouldRetry(t *testing.T) {
+	a := assert.NewAssert(t)
+
+	tests := []struct {
+		code   int
+		should bool
+	}{
+		{200, false},
+		{400, false},
+		{408, true},
+		{500, true},
+		{501, true},
+		{502, true},
+		{505, true},
+		{511, true},
+	}
+	for _, test := range tests {
+		a.Equal(test.should, web.ShouldRetry(test.code), "Retry test fails")
+	}
+}
+
+func TestIsTimeoutErr(t *testing.T) {
+	a := assert.NewAssert(t)
+
+	tests := []struct {
+		isTimeout bool
+		err       error
+		msg       string
+	}{
+		{false, errors.New("not timeout"), "Normal error is not"},
+		{false, &net.AddrError{}, "AddrError is not"},
+		{true, &net.DNSError{IsTimeout: true}, "DNSError is timeout"},
+	}
+
+	for _, test := range tests {
+		a.Equal(test.isTimeout, web.IsTimeoutErr(test.err), test.msg)
+	}
+}
+
+func TestBackoff_Next(t *testing.T) {
+	a := assert.NewAssert(t)
+
+	var (
+		minTimeout  = 10
+		maxTimeout  = 50
+		testBackoff = web.Backoff{
+			BaseSleep: minTimeout,
+			MaxSleep:  maxTimeout,
+		}
+	)
+
+	sleep0 := testBackoff.Next(0)
+	a.True(sleep0 >= minTimeout && sleep0 <= minTimeout*3, "First sleep is bounded")
+	sleep1 := testBackoff.Next(sleep0)
+	sleep2 := testBackoff.Next(sleep1)
+	sleep3 := testBackoff.Next(sleep2)
+	a.True(sleep1 >= minTimeout && sleep2 >= minTimeout, "Each sleep > base")
+	a.True(sleep2 <= maxTimeout && sleep3 <= maxTimeout, "Each sleep < max")
+}
+
+type retryTest struct {
+	closeTest
+	maxTries int
+	tries    int
+}
+
+func TestClientDo(t *testing.T) {
+	a := assert.NewAssert(t)
+
+	tests := []retryTest{
+		{
+			closeTest{
+				okHandler,
+				http.StatusOK,
+				okResp,
+				false,
+			},
+			3,
+			1,
+		},
+		{
+			closeTest{
+				DummyHandler(http.StatusInternalServerError, errResp),
+				http.StatusInternalServerError,
+				errResp,
+				false,
+			},
+			5,
+			5,
+		},
+	}
+
+	cl := web.NewClient()
+
+	for _, test := range tests {
+		server := httptest.NewServer(test.h)
+
+		req, err := http.NewRequest("GET", server.URL, nil)
+		if err != nil {
+			t.Errorf("Error new request: %s", err)
+			continue
+		}
+		n, status, body, err := cl.Do(req, test.maxTries)
+		if test.expectTimeout {
+			a.NotNil(err, "Should return timeout error")
+			a.Equal(true, web.IsTimeoutErr(err), "Should be a timeout")
+		} else {
+			a.NoError(err, "Request succeeds")
+			a.Equal(test.expectedCode, status, "Returns code")
+			a.Equal(test.expectedBody, body, "Returns body")
+		}
+		a.Equal(test.tries, n, "Report retried times")
+
+		server.Close()
+	}
+}
+
+type optionTest struct {
+	cl web.Client
+	retryTest
+}
+
+func TestClientOptions(t *testing.T) {
+	a := assert.NewAssert(t)
+
+	timeoutCl := web.NewClient(web.TimeoutOnly(),
+		web.WithHTTPClient(&http.Client{Timeout: 10 * time.Millisecond}),
+		web.WithBackoff(web.Backoff{BaseSleep: 100, MaxSleep: 200}))
+	// TODO: case for backoff?
+	tests := []optionTest{
+		{
+			timeoutCl,
+			retryTest{
+				closeTest{
+					okHandler,
+					http.StatusOK,
+					okResp,
+					false,
+				},
+				5,
+				1,
+			},
+		},
+		{
+			timeoutCl,
+			retryTest{
+				closeTest{
+					SleepHandler(50 * time.Millisecond),
+					http.StatusOK,
+					okResp,
+					true,
+				},
+				5,
+				5,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		server := httptest.NewServer(test.h)
+
+		req, err := http.NewRequest("GET", server.URL, nil)
+		if err != nil {
+			t.Errorf("Error new request: %s", err)
+			continue
+		}
+		n, status, body, err := test.cl.Do(req, test.maxTries)
+		if test.expectTimeout {
+			a.NotNil(err, "Should return timeout error")
+			a.Equal(true, web.IsTimeoutErr(err), "Should be a timeout")
+		} else {
+			a.NoError(err, "Request succeeds")
+			a.Equal(test.expectedCode, status, "Returns code")
+			a.Equal(test.expectedBody, body, "Returns body")
+		}
+		a.Equal(test.tries, n, "Report retried times")
+
+		server.Close()
+	}
+}
